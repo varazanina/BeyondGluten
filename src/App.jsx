61d50c6a01685c0df035c518caf3f99c1c49158b
varazanina <113165723+varazanina@users.noreply.github.com>
2023-10-30 09:45:34 +0100

    homepage moving

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import { HomePage } from './Pages/Components/HomePage'
+import { HomePage } from './Pages/HomePage'
 
 function App() {
 
