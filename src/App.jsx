02c85a579f6839f649bfb8648ed4764b02ab5e36
varazanina <113165723+varazanina@users.noreply.github.com>
2023-10-30 09:06:19 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import { HomePage } from './Pages/Components/HomePage'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
