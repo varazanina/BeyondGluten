adaf17a86053fb905f104a8554474f424b683ab8
varazanina <113165723+varazanina@users.noreply.github.com>
2023-10-31 07:17:09 +0100

    active icon classes

--- src/Pages/Components/Navigation.jsx
+++ src/Pages/Components/Navigation.jsx
@@ -1,20 +1,26 @@
 import "./navigation.css"
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation } from "react-router-dom"
 import Home from "../../assets/Home.png"
 import Create from "../../assets/Create.png"
 import Saved from "../../assets/Saved.png"
+import HoverHome from "../../assets/hoverHome.png"
+import HoverSaved from "../../assets/hoverSaved.png"
 
 export const Navigation = () => {
+  const location = useLocation();
+
   return (
     <nav className="navigation">
       <NavLink to="/">
-        <img src={ Home } alt="Home link" />
+        <img className={`icon ${location.pathname === '/' ? 'active' : ''}`} src={ Home } alt="Home link" />
+        <img className="filledIcon" src={ HoverHome } alt="Home link" />
       </NavLink>
       <NavLink to="/create">
         <img src={ Create } alt="Create a recipe" />
       </NavLink>
       <NavLink to="/saved">
-        <img src={ Saved } alt="Saved recipes" />
+        <img className={`icon ${location.pathname === '/saved' ? 'active' : ''}`} src={ Saved } alt="Saved recipes" />
+        <img className="filledIcon" src={ HoverSaved } alt="Saved recipes" />
       </NavLink>
     </nav>
   )
