da5ead94e02ed216123fe29c9c2a8a363681b017
Shiviara <113165919+Shiviara@users.noreply.github.com>
2023-11-03 14:25:31 +0100

    basic recipe card

--- src/Pages/Components/RecipeCard.jsx
+++ src/Pages/Components/RecipeCard.jsx
@@ -1,15 +1,22 @@
-import "./recipeCard.css"
-import { useNavigate } from "react-router-dom"
+import "./recipeCard.css";
+import { useNavigate } from "react-router-dom";
 
-export const RecipeCard = ({recipe}) => {
-    const navigate = useNavigate();
+export const RecipeCard = ({ recipe }) => {
+  const navigate = useNavigate();
 
   return (
-    <div className="recipeCard" onClick={() => navigate (`/recipes/${recipe.id}`)}>
+    <div
+      className="recipeCard"
+      onClick={() => navigate(`/recipes/${recipe.id}`)}
+    >
+      <div className="top-bar">
         <h4>{recipe.username}</h4>
-        <img className="picture" src={recipe.picture} alt="image" />
+      </div>
+      <img className="picture" src={recipe.picture} alt="image" />
+      <div className="post-content">
         <h1>{recipe.name}</h1>
-        <h3>{recipe.description}</h3>
+        <p>{recipe.description}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
