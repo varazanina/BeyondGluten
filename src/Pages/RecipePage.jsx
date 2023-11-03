68b7a4451f504e3da5bd4a6eb9a25d4e806e1171
varazanina <113165723+varazanina@users.noreply.github.com>
2023-11-03 11:31:31 +0100

    fetching on Recipe Page

--- src/Pages/RecipePage.jsx
+++ src/Pages/RecipePage.jsx
@@ -1,7 +1,25 @@
 import "./recipePage.css"
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
 
 export const RecipePage = () => {
+    const params = useParams();
+    const url = `https://beyond-gluten-default-rtdb.europe-west1.firebasedatabase.app/recipes/${params.recipeId}.json`;
+
+    const [recipe, setRecipe] = useState({});
+
+    useEffect (() => {
+        async function getRecipe() {
+            const response = await fetch(url);
+            const data = await response.json();
+            setRecipe(data);
+        }
+        getRecipe();
+    },);
+
   return (
-    <div>RecipePage</div>
+    <div>
+        <h1>{recipe.name}</h1>
+    </div>
   )
 }
