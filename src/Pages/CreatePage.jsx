c24b5198fe0b6661caacb86bf074f6904c19bcc7
Shiviara <113165919+Shiviara@users.noreply.github.com>
2023-11-03 10:30:49 +0100

    add steps and ingredients

--- src/Pages/CreatePage.jsx
+++ src/Pages/CreatePage.jsx
@@ -3,10 +3,40 @@ import { useNavigate } from "react-router-dom";
 
 export const CreatePage = () => {
   const [name, setName] = useState("");
-  const [image, setImage] = useState("");
+  const [picture, setPicture] = useState("");
   const [description, setDescription] = useState("");
-  const [ingredients, setIngredients] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+  const [steps, setSteps] = useState([]);
   const navigate = useNavigate();
+
+  //add new Ingredients by ChatGPT
+  const addIngredient = () => {
+    const newIngredient = {
+      name: "",
+      quantity: "",
+    };
+    setIngredients([...ingredients, newIngredient]);
+  };
+
+  const handleIngredientChange = (index, field, value) => {
+    const updatedIngredients = [...ingredients];
+    updatedIngredients[index][field] = value;
+    setIngredients(updatedIngredients);
+  };
+
+  //add steps by Marta
+  const addStep = () => {
+    const newStep = {
+      name: "",
+    };
+    setSteps([...steps, newStep]);
+  };
+
+  const handleStepChange = (index, field, value) => {
+    const updatedSteps = [...steps];
+    updatedSteps[index][field] = value;
+    setSteps(updatedSteps);
+  };
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -14,9 +44,10 @@ export const CreatePage = () => {
 
     const newPost = {
       name: name,
-      image: image,
+      picture: picture,
       description: description,
       ingredients: ingredients,
+      steps: steps,
       username: "glutenHater",
     };
     console.log(newPost);
@@ -29,13 +60,10 @@ export const CreatePage = () => {
       body: JSON.stringify(newPost),
     });
 
-    /*console.log(response);
-    navigate("/");*/
-
     if (response.ok) {
       navigate("/");
     } else {
-      console.log("Sth went wrong");
+      console.log("Something went wrong");
     }
   }
 
@@ -49,30 +77,63 @@ export const CreatePage = () => {
           placeholder="Type name of the dish here"
           value={name}
           onChange={(event) => setName(event.target.value)}
-        ></input>
-        <label>Image:</label>
+        />
+        <label>Picture:</label>
         <input
           type="url"
-          placeholder="Paste image url"
-          value={image}
-          onChange={(event) => setImage(event.target.value)}
-        ></input>
-        <img src=""></img>
+          placeholder="Paste picture url"
+          value={picture}
+          onChange={(event) => setPicture(event.target.value)}
+        />
+        <img src={picture} alt="" />
         <label>Description</label>
         <input
           type="text"
           placeholder="Tell us more about the dish"
           value={description}
           onChange={(event) => setDescription(event.target.value)}
-        ></input>
+        />
+        {/*add new Ingredients by ChatGPT*/}
         <label>Ingredients</label>
-        <input
-          type="text"
-          placeholder="Type name of the dish here"
-          value={ingredients}
-          onChange={(event) => setIngredients(event.target.value)}
-        ></input>
-        <button>Create</button>
+        {ingredients.map((ingredient, index) => (
+          <div key={index}>
+            <input
+              type="text"
+              placeholder="Ingredient name"
+              value={ingredient.name}
+              onChange={(e) =>
+                handleIngredientChange(index, "name", e.target.value)
+              }
+            />
+            <input
+              type="text"
+              placeholder="Quantity"
+              value={ingredient.quantity}
+              onChange={(e) =>
+                handleIngredientChange(index, "quantity", e.target.value)
+              }
+            />
+          </div>
+        ))}
+        <button type="button" onClick={addIngredient}>
+          Add Ingredient
+        </button>
+        {/*add new steps by Marta*/}
+        <label>Steps</label>
+        {steps.map((step, index) => (
+          <div key={index}>
+            <input
+              type="text"
+              placeholder="Step"
+              value={step.name}
+              onChange={(e) => handleStepChange(index, "name", e.target.value)}
+            />
+          </div>
+        ))}
+        <button type="button" onClick={addStep}>
+          Add Step
+        </button>
+        <button type="submit">Create</button>
       </form>
     </section>
   );
