9143ddf787f8ec6abf309d1b419bdd2ff1519ed1
PetyaVr <petya.vragova@gmail.com>
2023-11-08 19:35:19 +0100

    error-page

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,3 +1,4 @@
+//done by Petya
 import React from 'react';
 import { Link } from 'react-router-dom';
 import GoBack from '../assets/arrow_back.svg';
@@ -11,7 +12,7 @@ function ErrorPage() {
         <Link to="/">
         <img src={GoBack} alt="Go to the previous page" className="back-btn" />
         </Link>
-      <h2 className='heading404'>Ooops... the meal is not <br />ready yet!</h2>
+      <h2 className='heading404'>Ooops... the meal is not <br/>ready yet!</h2>
       <p className='smallheading404'>The page you are looking for was not found...</p>
       <img src="../src/assets/404_image.svg" alt="Sandwich Image" className="error" />
       <button className='primarybutton'>Go back to Home</button>
