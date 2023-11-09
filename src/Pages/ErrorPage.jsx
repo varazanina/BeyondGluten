51faed42f25175e5a4e6e51e9d78e9abf7ba9eef
varazanina <113165723+varazanina@users.noreply.github.com>
2023-11-09 09:47:38 +0100

    404

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,6 +1,6 @@
-import { Link, NavLink } from 'react-router-dom';
-import GoBack from '../assets/arrow_back.svg';
+import { Link } from 'react-router-dom';
 import { Navigation } from './Components/Navigation';
+import ErrorImage from "../assets/404_image.svg";
 import './ErrorPage.css';
 
 function ErrorPage() {
@@ -10,7 +10,7 @@ function ErrorPage() {
       <div className='general_margin'>
       <h2 className='heading404'>Ooops... the meal is not <br />ready yet!</h2>
       <p className='smallheading404'>The page you are looking for was not found...</p>
-      <img src="../src/assets/404_image.svg" alt="Sandwich Image" className="error" />
+      <img src={ ErrorImage } alt="Sandwich Image" className="error" />
       <Link to="/" >
       <button className='primarybutton' >Go back to Home</button>
       </Link>
